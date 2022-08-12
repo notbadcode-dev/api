44818a05736b06c898bebc7245540a37e94ede2f
NotBadCode <notbadcode.dev@gmail.com>
2022-08-12 15:05:28 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,12 @@ import app from "./app";
  * Start Express server.
  */
 const server = app.listen(app.get("port"), async () => {
-  console.log(
-    `  App is running at http://localhost:${app.get("port")} in ${app.get(
-      "env"
-    )} mode`
-  );
+  const environmentPort: number = app.get("port");
+  const environmentMode: string = app.get("env");
+
+  const apiUrl: string = `http://localhost:${environmentPort}`;
+
+  console.log(`  App is running at ${apiUrl} in ${environmentMode} mode`);
   console.log("  Press [Ctrl + C] to stop\n");
 });
 
